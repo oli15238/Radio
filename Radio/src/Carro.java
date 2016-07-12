27d6fe9ab88264d8253ca77f389bff89896947e4
oli15238 <oli15238@uvg.edu.gt>
2016-07-11 19:41:51 -0600

    Update Carro.java

--- Radio/src/Carro.java
+++ Radio/src/Carro.java
@@ -4,100 +4,45 @@ import java.util.*;
 /**
  * 
  */
-public class Carro {
-
-    /**
-     * 
-     */
-    public Carro() {
-    }
-
-    /**
-     * 
-     */
+public class Carro implements Radio{
+    public Carro() {}
     private boolean estado;
-
-    /**
-     * 
-     */
     private boolean frecuencia;
-
-    /**
-     * 
-     */
     private float emisora;
-
-    /**
-     * 
-     */
     private boolean entrada;
-
-    /**
-     * 
-     */
     private double botonesAM[];
-    
     private double botonesFM[];
     
     public void OnOff() {
-        // TODO implement here
-        return null;
+        
     }
 
-    /**
-     * @return
-     */
     public void AmFM() {
-        // TODO implement here
-        return null;
+        
     }
 
-    /**
-     * @return
-     */
     public void CambiarEmisora() {
-        // TODO implement here
-        return null;
+        
     }
 
-    /**
-     * @return
-     */
     public void GuardarEmisora() {
-        // TODO implement here
-        return null;
+        
     }
 
-    /**
-     * @return
-     */
     public void SeleccionarEmisora() {
-        // TODO implement here
-        return null;
+        
     }
 
-    /**
-     * @return
-     */
     public void GuardarSeleccionar() {
-        // TODO implement here
-        return null;
+        
     }
 
-    /**
-     * @return
-     */
     public void MenuOn() {
-        // TODO implement here
-        return null;
+        
     }
 
-    /**
-     * @return
-     */
     public void MenuOff() {
-        // TODO implement here
-        return null;
+        
     }
 
 }
